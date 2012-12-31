e4564e9fb33dcd4a8c817d354c03ab87d91233ea
Yannick Scherer <yannick.scherer@googlemail.com>
2012-12-31 02:26:14 +0100

    Version 0.1.0: fixed description in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject combit "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Intuitive and concise processing of fixed-size Data Containers."
+  :url "http://github.com/xsc/combit"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]])
