045c9c81bd5b90e5a1e87b177e4ddc6900ae54a3
Yannick Scherer <yannick.scherer@gmail.com>
2013-01-05 04:01:51 +0100

    Version 0.3.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject combit "0.2.0"
+(defproject combit "0.3.0"
   :description "Intuitive and concise processing of fixed-size Data Containers."
   :url "http://github.com/xsc/combit"
   :license {:name "Eclipse Public License"
